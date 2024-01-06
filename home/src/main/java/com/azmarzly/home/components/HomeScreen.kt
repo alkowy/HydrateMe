fc6fbdf9a04677bbe8ca2b82dc1c9c46472e8ed4
Aleksander Zmarzły <alek.zmarzly@gmail.com>
2024-01-06 15:04:10 +0100

    Add ellipsis to welcome header on the home screen

--- home/src/main/java/com/azmarzly/home/components/HomeScreen.kt
+++ home/src/main/java/com/azmarzly/home/components/HomeScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -71,9 +72,12 @@ fun HomeScreenContent(
             modifier = Modifier.fillMaxWidth()
         ) {
             Text(
+                modifier = Modifier.weight(1f),
                 text = "${stringResource(R.string.hello_header)} ${homeState.userData?.name ?: ""}!",
                 style = MaterialTheme.typography.h2,
                 color = MaterialTheme.colors.onSurface,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 2
             )
             AsyncImage(
                 model = ImageRequest.Builder(LocalContext.current)
